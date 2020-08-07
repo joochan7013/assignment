8ebf72677a702f9cb7f4cf80e8a4842a5365b597
unknown <joochan7013@gmail.com>
2020-08-07 02:40:21 -0400

    Assignmet 3 revised

--- model/user.js
+++ model/user.js
@@ -30,21 +30,3 @@ const userSchema = new Schema({
 
 const usermod = mongoose.model('user', userSchema);
 module.exports = usermod;
-
-module.exports.getUserByEmail = function(inEmail){
-    return new Promise((resolve,reject)=>{
-        
-        usermod.find({email: inEmail}) //gets all and returns an array. Even if 1 or less entries
-        .exec() //tells mongoose that we should run this find as a promise.
-        .then((returnedUser)=>{
-            if(returnedUser.length !=0 )
-            //resolve(filteredMongoose(returnedStudents));
-                resolve(returnedUser.map(item=>item.toObject()));
-            else
-                reject("No User found");
-        }).catch((err)=>{
-                console.log("Error Retriving User:"+err);
-                reject(err);
-        });
-    });
-}
